37d1483e8234905595042e95ed1b03a21290efd2
Lacerda02 <114315250+Venancio-Sancho@users.noreply.github.com>
2023-03-31 20:22:16 +0200

    a

---
+++ user/deleteQuery.php
@@ -0,0 +1,20 @@
+<?php
+	include_once("../db.php");
+	// include_once("../header.php");
+
+	
+	$id = $_GET['id'];
+
+	$query = mysqli_query($conectar, "DELETE FROM users WHERE id = '$id'");
+	
+	/* PDO
+		$stmt =$pdo->prepare("delete from user where id=" . $_GET['id']);
+		$stmt->execute();
+		
+		ou
+		$stmt = $pdo->prepare("DELETE FROM users WHERE id=?");
+		$stmt->execute([$id]);
+	*/
+	
+	header("location: deleteMsg.php");
+?>
